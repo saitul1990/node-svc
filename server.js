a510703e7c3828f5d00aaddf228e935947e79ab5
Charles T. Betz <betz4871@stthomas.edu>
2020-10-25 20:18:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,9 @@
 // to do: this should be a command-line parameter that lets the app know how it is running
 //const arrNodes = [ "localhost" ]                                    // for testing on GCS
 //const arrNodes = [ "node-svc-01" ]                                  // for 1 VM
-//const arrNodes = [ "node-svc-01", "node-svc-02" ]                   // for 2 VMs
+const arrNodes = [ "node-svc-01", "node-svc-02" ]                   // for 2 VMs
 //const arrNodes = [ "node-svc-01", "node-svc-02" , "node-svc-03" ]   // for 3 VMs
-const arrNodes = [ process.env.NODE_SVC_PUBLIC_SERVICE_HOST  ];       //  use this for K8S
+//const arrNodes = [ process.env.NODE_SVC_PUBLIC_SERVICE_HOST  ];       //  use this for K8S
 
 console.log("service host is " + arrNodes[0]);
 
